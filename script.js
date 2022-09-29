6d857a62a0bf1ef36f33a304acbe3175385e69e7
Jesus Gonzalez <jesusgon46540@gmail.com>
2022-09-29 00:45:11 -0700

    did what i could

--- script.js
+++ script.js
@@ -1,106 +1,105 @@
-window.addEventListener(`load`, function(){
-    setTimeout(
+window.addEventListener("load", function(){
+    this.setTimeout(
         function open(event){
-            document.querySelector(`.popup`).style.display = 
-            `block`;
+            document.querySelector(".popup").style.
+            display = "block";
         },
-        2000
+        1000
     )
 });
 
-document.querySelector(`#close`).addEventListener
-(`click`, function(){
+document.querySelector("#close").addEventListener
+("click" ,function(){
     document.querySelector(".popup").style.display =
     "none";
 });
 
 
-
-let firstName = document.querySelector('#first-name').value;
-let lastName = document.querySelector(`#last-name`).value;
-
-
-if (firstName.length + lastName.length <= 2){
-    console.log('incomplete First Name!')
-function createErrorItem(name, idName) {
-    let error = document.querySelector(`#errors`)
-    let li = document.createElement('li');
-    li.textContent = name;
-    error.appendChild(li);
-};
+// let firstName = document.querySelector('#first-name').value;
+// let lastName = document.querySelector(`#last-name`).value;
 
 
-let lastName = document.querySelector(`#last-name`);
-lastName.addEventListener('change', function (event) {
-            let firstName = document.querySelector(`#first-name`).value;
-            let last = document.querySelector("#last-name").value;
-            if (firstName.length + last.length <= 2){
-                console.log('Incomplete Name');
-                createErrorItem(`incomplete First Name!`);
-            };
-        }
-);
-
-let areaNumbers = document.querySelector(`#phone-area-code`).value;
-let middleNumbers = document.querySelector(`#phone-prefix`).value;
-let thirdNumber = document.querySelector(`#phone-line-number`).value;
+// if (firstName.length + lastName.length <= 2){
+//     console.log('incomplete First Name!')
+// function createErrorItem(name, idName) {
+//     let error = document.querySelector(`#errors`)
+//     let li = document.createElement('li');
+//     li.textContent = name;
+//     error.appendChild(li);
+// };
 
 
-if (areaNumbers.length + middleNumbers.length + thirdNumbers.length != 10 ){
-    console.log('Incomplete number!')
-};
-let thirdNumbers = document.querySelector(`#phone-line-number`);
-thirdNumbers.addEventListener('change', function (event)
- { 
-        let areaNumbers = document.querySelector(`#phone-area-code`).value;
-e
-        let middleNumbers = document.querySelector(`#phone-prefix`).value;
+// let lastName = document.querySelector(`#last-name`);
+// lastName.addEventListener('change', function (event) {
+//             let firstName = document.querySelector(`#first-name`).value;
+//             let last = document.querySelector("#last-name").value;
+//             if (firstName.length + last.length <= 2){
+//                 console.log('Incomplete Name');
+//                 createErrorItem(`incomplete First Name!`);
+//             };
+//         }
+// );
 
-        let thirdNumbers = document.querySelector(`#phone-line-number`).value;
+// let areaNumbers = document.querySelector(`#phone-area-code`).value;
+// let middleNumbers = document.querySelector(`#phone-prefix`).value;
+// let thirdNumber = document.querySelector(`#phone-line-number`).value;
 
 
-        if (areatNumbers.length + middleNumbers.length + thirdNumbers.length != 10 ){
-            console.log('Incomplete Number!');
-            createErrorItem(`Invalid Phone Number`);
-        }
-});
+// if (areaNumbers.length + middleNumbers.length + thirdNumbers.length != 10 ){
+//     console.log('Incomplete number!')
+// };
+// let thirdNumbers = document.querySelector(`#phone-line-number`);
+// thirdNumbers.addEventListener('change', function (event)
+//  { 
+//         let areaNumbers = document.querySelector(`#phone-area-code`).value;
+// e
+//         let middleNumbers = document.querySelector(`#phone-prefix`).value;
 
-let courseSelect = document.querySelector(`#course-select`).value;
+//         let thirdNumbers = document.querySelector(`#phone-line-number`).value;
 
-if (courseSelect.value = `Select One`) {
+
+//         if (areatNumbers.length + middleNumbers.length + thirdNumbers.length != 10 ){
+//             console.log('Incomplete Number!');
+//             createErrorItem(`Invalid Phone Number`);
+//         }
+// });
+
+// let courseSelect = document.querySelector(`#course-select`).value;
+
+// if (courseSelect.value = `Select One`) {
     
-    console.log('Select an option!');
-};
+//     console.log('Select an option!');
+// };
 
-let submit = document.querySelector('#application-form');
-submit.addEventListener('submit', function (event) {
-    console.log('Submitting')
-
-
-    let courseSelect = document.querySelector(`#course-select`);
-
-    let selectedtext = courseSelect.options[courseSelect.selectedIndex].text;
-    if (selectedtext === "Select One") 
-
-    {
-       console.log('Select an option!');
+// let submit = document.querySelector('#application-form');
+// submit.addEventListener('submit', function (event) {
+//     console.log('Submitting')
 
 
-        createErrorItem(`Please Select a Course`);
-        event.preventDefault()
-    }
-    let pastExperience = document.querySelector(`#past-experience`).value;
+//     let courseSelect = document.querySelector(`#course-select`);
 
-    if (pastExperience.length <= 50 ){
-        console.log('Please give us more details )');
-        createErrorItem(`Please give us more Information`);
-        event.preventDefault()
-    };
-});
+//     let selectedtext = courseSelect.options[courseSelect.selectedIndex].text;
+//     if (selectedtext === "Select One") 
 
-let pastExperience = document.querySelector(`#past-experience`).value;
+//     {
+//        console.log('Select an option!');
 
-if (pastExperience <= 50 ){
-    console.log('Please give us more details :)')
-} };
+
+//         createErrorItem(`Please Select a Course`);
+//         event.preventDefault()
+//     }
+//     let pastExperience = document.querySelector(`#past-experience`).value;
+
+//     if (pastExperience.length <= 50 ){
+//         console.log('Please give us more details )');
+//         createErrorItem(`Please give us more Information`);
+//         event.preventDefault()
+//     };
+// });
+
+// let pastExperience = document.querySelector(`#past-experience`).value;
+
+// if (pastExperience <= 50 ){
+//     console.log('Please give us more details :)')
+// } };
 
